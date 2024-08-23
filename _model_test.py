122925812c8692d80f2b4a69f8992eb2d0d1f674
AdwayB <adwaybyju@gmail.com>
2024-08-24 00:14:22 +0530

    add first prototype

---
+++ _model_test.py
@@ -0,0 +1,197 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.utils.class_weight import compute_class_weight
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, precision_recall_fscore_support
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+
+
+real_life_data = pd.read_csv('./datasets/normalized_landmarks_dataset_a.csv')
+staged_data = pd.read_csv('./datasets/normalized_landmarks_dataset_b.csv').sample(frac=1/4, random_state=42)
+
+data = pd.concat([real_life_data, staged_data], axis=0, ignore_index=True)
+
+print(data.head())
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+label_encoder = LabelEncoder()
+
+X = data.iloc[:, 3:].values  # All landmark columns
+y = label_encoder.fit_transform(data['label'].astype(str).values)
+
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+X_train_tensor = torch.tensor(X_train, dtype=torch.float32).to(device)
+y_train_tensor = torch.tensor(y_train, dtype=torch.long).to(device)
+
+X_val_tensor = torch.tensor(X_val, dtype=torch.float32).to(device)
+y_val_tensor = torch.tensor(y_val, dtype=torch.long).to(device)
+
+X_test_tensor = torch.tensor(X_test, dtype=torch.float32).to(device)
+y_test_tensor = torch.tensor(y_test, dtype=torch.long).to(device)
+
+X_tensor = torch.tensor(X, dtype=torch.float32).to(device)
+y_tensor = torch.tensor(y, dtype=torch.long).to(device)
+
+train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+val_dataset = TensorDataset(X_val_tensor, y_val_tensor)
+test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, drop_last=True)
+val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False, drop_last=True)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False, drop_last=True)
+
+
+class GestureClassifier(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(GestureClassifier, self).__init__()
+
+        self.fc1 = nn.Linear(input_size, 512)
+        self.bn1 = nn.BatchNorm1d(512)
+
+        self.fc2 = nn.Linear(512, 256)
+        self.bn2 = nn.BatchNorm1d(256)
+
+        # self.fc3 = nn.Linear(512, 256)
+        # self.bn3 = nn.BatchNorm1d(256)
+        #
+        self.fc3 = nn.Linear(256, 128)
+        self.bn3 = nn.BatchNorm1d(128)
+
+        self.fc4 = nn.Linear(128, num_classes)
+
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = self.dropout(x)
+
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = self.dropout(x)
+
+        x = F.relu(self.bn3(self.fc3(x)))
+        x = self.dropout(x)
+        #
+        # x = F.relu(self.bn4(self.fc4(x)))
+        # x = self.dropout(x)
+
+        x = self.fc4(x)
+        return x
+
+
+input_size = X.shape[1]  # Number of input features (landmarks)
+num_classes = len(set(y))  # Number of unique classes
+model = GestureClassifier(input_size, num_classes)
+model = model.to(device)
+
+class_weights = compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
+class_weights = torch.tensor(class_weights, dtype=torch.float).to(device)
+
+criterion = nn.CrossEntropyLoss(weight=class_weights)
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+num_epochs = 50
+# scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2)
+
+best_val_loss = float('inf')
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    scheduler.step()
+
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    val_loss /= len(val_loader)
+    val_accuracy = 100 * correct / total
+
+    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(train_loader): .4f}, "
+          f"Validation Loss: {val_loss: .4f}, Validation Accuracy: {val_accuracy: .2f}%")
+
+    # Save the model if validation loss decreases
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save(model.state_dict(), 'best_model.pth')
+
+
+model.eval()
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Training Accuracy: {100 * correct / total}%')
+
+model.load_state_dict(torch.load('best_model.pth'))
+
+model.eval()
+all_preds = []
+all_labels = []
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        all_preds.extend(predicted.cpu().numpy())
+        all_labels.extend(labels.cpu().numpy())
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+
+test_accuracy = 100 * correct / total
+print(f'Test Accuracy: {test_accuracy: .2f}%')
+
+precision, recall, f1, _ = precision_recall_fscore_support(all_labels, all_preds, average=None)
+
+for i, label in enumerate(label_encoder.classes_):
+    print(f"Class: {label}, Test Precision: {precision[i]: .4f}, Test Recall: {recall[i]: .4f}, "
+          f"Test F1 Score: {f1[i]: .4f}")
+
+cm = confusion_matrix(all_labels, all_preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=label_encoder.classes_)
+disp.plot(cmap='Blues')
+plt.savefig('confusion_matrix.png')
